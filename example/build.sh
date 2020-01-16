12762a14da624cf5eb74edf11900e5e811afb966
Patryk Dwórznik <patryk@dworznik.com>
2020-01-16 22:52:08 +0900

    Fix path to the binary

--- example/build.sh
+++ example/build.sh
@@ -1,2 +1,2 @@
-databundler -pkg sevensummits -data sevensummits.csv -schema schema.yaml -output sevensummits.go
+../databundler -pkg sevensummits -data sevensummits.csv -schema schema.yaml -output sevensummits.go
 gofmt -w sevensummits.go
